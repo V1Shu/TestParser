341e4039aa6476ea1ac9533f3c5211d3b437bc63
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-03-19 17:52:34 +0200

    try using jsoup

--- src/main/java/ru/example/parser/Application.java
+++ src/main/java/ru/example/parser/Application.java
@@ -1,7 +1,23 @@
 package ru.example.parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Application {
+    private static final String TEST_PAGE = "https://flashdeals.aliexpress.com/en.htm?";
+
     public static void main(String[] args) {
 
+        Document document = null;
+        try {
+            document = Jsoup.connect(TEST_PAGE).get();
+            System.out.println(document);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
