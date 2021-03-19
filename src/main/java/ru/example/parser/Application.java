824f6a30dc2a3c8ce5879bf8758718fe2506b495
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-03-19 22:39:20 +0200

    try use apache

--- src/main/java/ru/example/parser/Application.java
+++ src/main/java/ru/example/parser/Application.java
@@ -1,23 +1,13 @@
 package ru.example.parser;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
+import org.apache.http.client.methods.HttpGet;
 
 public class Application {
+    private static final String PAGE_TO_PARSE = "https://gpsfront.aliexpress.com/getRecommendingResults.do?callback=jQuery18308045810847012902_1616169506790&widget_id=5547572&platform=pc&limit=12&offset=12&phase=1&productIds2Top=&postback=cc12216d-c90c-4072-a71b-7eddfdffae7c&_=1616169515462";
     private static final String TEST_PAGE = "https://flashdeals.aliexpress.com/en.htm?";
 
     public static void main(String[] args) {
+        HttpGet httpGet =new HttpGet("PAGE_TO_PARSE");
 
-        Document document = null;
-        try {
-            document = Jsoup.connect(TEST_PAGE).get();
-            System.out.println(document);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
